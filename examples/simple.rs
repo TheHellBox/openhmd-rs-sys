d77afac68f47ff91d090b8da6dbac99b5599ad47
TheHellBox <thehellbox11@gmail.com>
2019-06-23 16:40:46 +0500

    Use rustfmt

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,22 +1,22 @@
-extern crate openhmd_rs_sys;
 extern crate libc;
+extern crate openhmd_rs_sys;
+use libc::{c_float, c_int};
 use openhmd_rs_sys::*;
-use libc::{c_int, c_float};
 use std::ffi::CStr;
 use std::time::Duration;
-fn main(){
-    unsafe{
+fn main() {
+    unsafe {
         let context = ohmd_ctx_create();
         let num_devices = ohmd_ctx_probe(context);
         println!("num devices: {}", num_devices);
         println!("");
-        for i in 0 .. num_devices {
+        for i in 0..num_devices {
             let vendor = ohmd_list_gets(context, i, ohmd_string_value::OHMD_VENDOR);
             let product = ohmd_list_gets(context, i, ohmd_string_value::OHMD_PRODUCT);
             let path = ohmd_list_gets(context, i, ohmd_string_value::OHMD_PATH);
             let mut out: [c_int; 1] = [0; 1];
             ohmd_list_geti(context, i, ohmd_int_value::OHMD_DEVICE_CLASS, &mut out);
-            let device_class_s = [ "HMD", "Controller", "Generic Tracker", "Unknown" ];
+            let device_class_s = ["HMD", "Controller", "Generic Tracker", "Unknown"];
             let class = out[0] as usize;
             ohmd_list_geti(context, i, ohmd_int_value::OHMD_DEVICE_FLAGS, &mut out);
             let flags = out[0];
@@ -24,39 +24,109 @@ fn main(){
             println!("  vendor:  {}", CStr::from_ptr(vendor).to_str().unwrap());
             println!("  product: {}", CStr::from_ptr(product).to_str().unwrap());
             println!("  path:    {}", CStr::from_ptr(path).to_str().unwrap());
-            println!("  class:   {}", device_class_s[if class > 3 { 3 } else { class }]);
+            println!(
+                "  class:   {}",
+                device_class_s[if class > 3 { 3 } else { class }]
+            );
             println!("  flags:   {:02x}", flags);
-            println!("    null device:         {}", if flags & OHMD_DEVICE_FLAGS_NULL_DEVICE != 0 { "yes" } else { "no" });
-            println!("    rotational tracking: {}", if flags & OHMD_DEVICE_FLAGS_ROTATIONAL_TRACKING != 0 { "yes" } else { "no" });
-            println!("    positional tracking: {}", if flags & OHMD_DEVICE_FLAGS_POSITIONAL_TRACKING != 0 { "yes" } else { "no" });
-            println!("    left controller:     {}", if flags & OHMD_DEVICE_FLAGS_LEFT_CONTROLLER != 0 { "yes" } else { "no" });
-            println!("    right controller:    {}", if flags & OHMD_DEVICE_FLAGS_RIGHT_CONTROLLER != 0 { "yes" } else { "no" });
+            println!(
+                "    null device:         {}",
+                if flags & OHMD_DEVICE_FLAGS_NULL_DEVICE != 0 {
+                    "yes"
+                } else {
+                    "no"
+                }
+            );
+            println!(
+                "    rotational tracking: {}",
+                if flags & OHMD_DEVICE_FLAGS_ROTATIONAL_TRACKING != 0 {
+                    "yes"
+                } else {
+                    "no"
+                }
+            );
+            println!(
+                "    positional tracking: {}",
+                if flags & OHMD_DEVICE_FLAGS_POSITIONAL_TRACKING != 0 {
+                    "yes"
+                } else {
+                    "no"
+                }
+            );
+            println!(
+                "    left controller:     {}",
+                if flags & OHMD_DEVICE_FLAGS_LEFT_CONTROLLER != 0 {
+                    "yes"
+                } else {
+                    "no"
+                }
+            );
+            println!(
+                "    right controller:    {}",
+                if flags & OHMD_DEVICE_FLAGS_RIGHT_CONTROLLER != 0 {
+                    "yes"
+                } else {
+                    "no"
+                }
+            );
             println!("");
         }
 
         println!("opening device: {}", 0);
         let device = ohmd_list_open_device(context, 0);
         let mut out: [c_int; 1] = [0];
-        ohmd_device_geti(device, ohmd_int_value::OHMD_SCREEN_HORIZONTAL_RESOLUTION, &mut out);
+        ohmd_device_geti(
+            device,
+            ohmd_int_value::OHMD_SCREEN_HORIZONTAL_RESOLUTION,
+            &mut out,
+        );
         let width = out[0];
-        ohmd_device_geti(device, ohmd_int_value::OHMD_SCREEN_VERTICAL_RESOLUTION, &mut out);
+        ohmd_device_geti(
+            device,
+            ohmd_int_value::OHMD_SCREEN_VERTICAL_RESOLUTION,
+            &mut out,
+        );
         let height = out[0];
         let mut out: [c_float; 16] = [0.0; 16];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_SCREEN_HORIZONTAL_SIZE, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_SCREEN_HORIZONTAL_SIZE,
+            &mut out,
+        );
         let hsize = out[0];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_SCREEN_VERTICAL_SIZE, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_SCREEN_VERTICAL_SIZE,
+            &mut out,
+        );
         let vsize = out[0];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_LENS_HORIZONTAL_SEPARATION, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_LENS_HORIZONTAL_SEPARATION,
+            &mut out,
+        );
         let lens_separation = out[0];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_LENS_VERTICAL_POSITION, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_LENS_VERTICAL_POSITION,
+            &mut out,
+        );
         let lens_vcenter = out[0];
         ohmd_device_getf(device, ohmd_float_value::OHMD_LEFT_EYE_FOV, &mut out);
         let left_eye_fov = out[0];
         ohmd_device_getf(device, ohmd_float_value::OHMD_RIGHT_EYE_FOV, &mut out);
         let right_eye_fov = out[0];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_LEFT_EYE_ASPECT_RATIO, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_LEFT_EYE_ASPECT_RATIO,
+            &mut out,
+        );
         let left_eye_aspect = out[0];
-        ohmd_device_getf(device, ohmd_float_value::OHMD_RIGHT_EYE_ASPECT_RATIO, &mut out);
+        ohmd_device_getf(
+            device,
+            ohmd_float_value::OHMD_RIGHT_EYE_ASPECT_RATIO,
+            &mut out,
+        );
         let right_eye_aspect = out[0];
 
         println!("resolution:              {} x {}", width, height);
@@ -70,12 +140,18 @@ fn main(){
         println!("right eye aspect:        {:.6}", right_eye_aspect);
         println!("");
 
-        for _i in 0 .. 10000 {
+        for _i in 0..10000 {
             ohmd_ctx_update(context);
             ohmd_device_getf(device, ohmd_float_value::OHMD_ROTATION_QUAT, &mut out);
-            println!("rotation quat:           {:.6} {:.6} {:.6} {:.6}", out[0], out[1], out[2], out[3]);
+            println!(
+                "rotation quat:           {:.6} {:.6} {:.6} {:.6}",
+                out[0], out[1], out[2], out[3]
+            );
             ohmd_device_getf(device, ohmd_float_value::OHMD_POSITION_VECTOR, &mut out);
-            println!("position vec:            {:.6} {:.6} {:.6}", out[0], out[1], out[2]);
+            println!(
+                "position vec:            {:.6} {:.6} {:.6}",
+                out[0], out[1], out[2]
+            );
             std::thread::sleep(Duration::from_millis(10));
         }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 extern crate libc;
-use libc::{c_char, c_int, c_float, c_void};
+use libc::{c_char, c_float, c_int, c_void};
 #[repr(C)]
 pub struct ohmd_context {
-    __private: c_void
+    __private: c_void,
 }
 #[repr(C)]
 pub struct ohmd_device {
-    __private: c_void
+    __private: c_void,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_float_value{
+pub enum ohmd_float_value {
     OHMD_ROTATION_QUAT = 1,
     OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX = 2,
     OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX = 3,
@@ -34,34 +34,34 @@ pub enum ohmd_float_value{
     OHMD_EXTERNAL_SENSOR_FUSION = 19,
     OHMD_UNIVERSAL_DISTORTION_K = 20,
     OHMD_UNIVERSAL_ABERRATION_K = 21,
-    OHMD_CONTROLS_STATE = 22
+    OHMD_CONTROLS_STATE = 22,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_status{
+pub enum ohmd_status {
     OHMD_S_OK = 0,
     OHMD_S_UNKNOWN_ERROR = -1,
     OHMD_S_INVALID_PARAMETER = -2,
     OHMD_S_UNSUPPORTED = -3,
     OHMD_S_INVALID_OPERATION = -4,
-    OHMD_S_USER_RESERVED = -16384
+    OHMD_S_USER_RESERVED = -16384,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_string_value{
+pub enum ohmd_string_value {
     OHMD_VENDOR = 0,
     OHMD_PRODUCT = 1,
-    OHMD_PATH = 2
+    OHMD_PATH = 2,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_string_description{
+pub enum ohmd_string_description {
     OHMD_GLSL_DISTORTION_VERT_SRC = 0,
     OHMD_GLSL_DISTORTION_FRAG_SRC = 1,
     OHMD_GLSL_330_DISTORTION_VERT_SRC = 2,
@@ -71,7 +71,7 @@ pub enum ohmd_string_description{
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_control_hint{
+pub enum ohmd_control_hint {
     OHMD_GENERIC = 0,
     OHMD_TRIGGER = 1,
     OHMD_TRIGGER_CLICK = 2,
@@ -84,34 +84,34 @@ pub enum ohmd_control_hint{
     OHMD_BUTTON_A = 9,
     OHMD_BUTTON_B = 10,
     OHMD_BUTTON_X = 11,
-    OHMD_BUTTON_Y = 12
+    OHMD_BUTTON_Y = 12,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_control_type{
+pub enum ohmd_control_type {
     OHMD_DIGITAL = 0,
-    OHMD_ANALOG = 1
+    OHMD_ANALOG = 1,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_int_value{
+pub enum ohmd_int_value {
     OHMD_SCREEN_HORIZONTAL_RESOLUTION = 0,
     OHMD_SCREEN_VERTICAL_RESOLUTION = 1,
     OHMD_DEVICE_CLASS = 2,
     OHMD_DEVICE_FLAGS = 3,
     OHMD_CONTROL_COUNT = 4,
     OHMD_CONTROLS_HINTS = 5,
-    OHMD_CONTROLS_TYPES = 6
+    OHMD_CONTROLS_TYPES = 6,
 }
 
 #[repr(C)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy)]
-pub enum ohmd_device_class{
+pub enum ohmd_device_class {
     OHMD_DEVICE_CLASS_HMD = 0,
     OHMD_DEVICE_CLASS_CONTROLLER = 1,
     OHMD_DEVICE_CLASS_GENERIC_TRACKER = 2,
@@ -124,18 +124,39 @@ pub const OHMD_DEVICE_FLAGS_LEFT_CONTROLLER: c_int = 8;
 pub const OHMD_DEVICE_FLAGS_RIGHT_CONTROLLER: c_int = 16;
 
 #[link(name = "openhmd")]
-extern {
+extern "C" {
     pub fn ohmd_ctx_create() -> &'static ohmd_context;
     pub fn ohmd_ctx_destroy(ctx: &ohmd_context);
     pub fn ohmd_ctx_get_error(ctx: &ohmd_context) -> *const c_char;
     pub fn ohmd_ctx_probe(ctx: &ohmd_context) -> c_int;
     pub fn ohmd_ctx_update(ctx: &ohmd_context);
     pub fn ohmd_gets(stype: ohmd_string_description, out: &mut *const c_char) -> c_int;
-    pub fn ohmd_device_getf(device: &ohmd_device, otype: ohmd_float_value, out: &mut [c_float; 16]) -> c_int;
-    pub fn ohmd_device_setf(device: &ohmd_device, otype: ohmd_float_value, float: &[c_float; 16]) -> c_int;
+    pub fn ohmd_device_getf(
+        device: &ohmd_device,
+        otype: ohmd_float_value,
+        out: &mut [c_float; 16],
+    ) -> c_int;
+    pub fn ohmd_device_setf(
+        device: &ohmd_device,
+        otype: ohmd_float_value,
+        float: &[c_float; 16],
+    ) -> c_int;
     pub fn ohmd_list_open_device(ctx: &ohmd_context, index: c_int) -> &'static ohmd_device;
-    pub fn ohmd_list_gets(ctx: &ohmd_context, index: c_int, otype: ohmd_string_value) -> *const c_char;
-    pub fn ohmd_list_geti(ctx: &ohmd_context, index: c_int, otype: ohmd_int_value, out: &mut [c_int; 1]) -> c_int;
-    pub fn ohmd_device_geti(device: &ohmd_device, otype: ohmd_int_value, out: &mut [c_int; 1]) -> c_int;
+    pub fn ohmd_list_gets(
+        ctx: &ohmd_context,
+        index: c_int,
+        otype: ohmd_string_value,
+    ) -> *const c_char;
+    pub fn ohmd_list_geti(
+        ctx: &ohmd_context,
+        index: c_int,
+        otype: ohmd_int_value,
+        out: &mut [c_int; 1],
+    ) -> c_int;
+    pub fn ohmd_device_geti(
+        device: &ohmd_device,
+        otype: ohmd_int_value,
+        out: &mut [c_int; 1],
+    ) -> c_int;
     pub fn ohmd_close_device(device: &ohmd_device) -> c_int;
 }
